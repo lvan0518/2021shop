1ab5821529d7d539dd5ddbd0148382e834b1c9f4
lvan0518 <3270698153@qq.com>
2021-01-25 11:05:07 +0800

    'firstDay'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="home" class="wrapper">
+    <navbar class="home-nav">
+      <div slot="center">购物街</div>
+    </navbar>
+  </div>
+</template>
+
+<script>
+  import navbar from '../../components/common/navBar/NavBar'
+  export default {
+    name:"Home",
+    components:{
+      navbar
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color:var(--color-tint);
+    color:#fff;
+  }
+</style>
