57b34a13984890ffacd74145d3834dc15bbddf40
lvan0518 <3270698153@qq.com>
2021-01-27 14:14:04 +0800

    'secondday'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,22 +3,137 @@
     <navbar class="home-nav">
       <div slot="center">购物街</div>
     </navbar>
+    <home-swiper :banners="bannerList"/>
+    <recommend  :recommends="recommendList"/>
+    <feature-view />
+    <tab-control :titles="titleList" class="tab-control"/>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
   import navbar from '../../components/common/navBar/NavBar'
+  import TabControl from '../../components/content/tabControl/TabControl'
+
+  import HomeSwiper from './childComps/HomeSwiper'
+  import Recommend from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
+
+  import {getHomeMultiData,getHomeGoods} from "../../network/home";
   export default {
     name:"Home",
     components:{
-      navbar
+      navbar,
+      HomeSwiper,
+      Recommend,
+      FeatureView,
+      TabControl
+    },
+    data() {
+      return {
+        bannerList:[],
+        recommendList:[],
+        titleList:["流行","新款","精选"],
+        goods:{
+          'pop':{page:0,list:[]},
+          'new':{page:0,list:[]},
+          'sell':{page:0,list:[]},
+        }
+      }
+    },
+    created() {
+     this.getMulitiData();
+     this.getHomeGoods('pop');
+     this.getHomeGoods('new');
+     this.getHomeGoods('sell');
+    },
+    methods:{
+      getMulitiData() {
+        getHomeMultiData().then(resp => {
+          if(resp.data) {
+            //获取轮播图数据
+            this.bannerList = resp.data.banner.list;
+            this.recommendList = resp.data.recommend.list;
+          }
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page+1;
+        getHomeGoods(type,page).then(resp => {
+          this.goods[type].list .push(...resp.data.list);
+          this.goods[type].list .page+=1;
+        })
+      }
     }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color:var(--color-tint);
     color:#fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
